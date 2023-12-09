a097598a1ce091901cd9bb97049fe6bf55cb5c55
kadirsenol <kdrsnl_61@hotmail.com>
2023-12-09 13:01:28 +0300

    selectcommand

--- SelectQuery.sql
+++ SelectQuery.sql
@@ -16,3 +16,27 @@ Select *from Customers where Country='Germany' or country='France' Order by coun
 
 --Almanya ve Fransada ki müşterilerin listesini ülkeye ve sehre göre sırala
 Select *from Customers where Country='Germany' or country='France' Order by country, city 
+
+
+select *from products
+
+--fiyatı 30$ ve üzeri olan ürünlerin listesi
+select * from products where Unitprice >=30
+select Unitprice from products where Unitprice >=30 -- Sadece fiyat tablosu olarak 30 ve üzerileri çapırmak
+
+--fiyatı en ucuz olan ürünlerin listesi
+select * from products order  by unitprice
+
+--fiyatı en pahalı olan ürünlerin listesi
+select * from products order  by unitprice DESC
+
+-- Order(sipariş) tablosunu sorgulayın
+select *from orders
+
+--orderıd si şu olan siparişi getir.
+select * from orders where orderıd=10248
+
+--order details tablosunun ilk 10 kaydı(tablo isminde boşluk olduğu için köşeli parantez kullanlır)
+select top 10 *from [Order Details]
+
+--
